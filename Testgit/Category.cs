b359c9f5b9790e41e769d3baa2a4f3aaf939f977
Abdelrahman Khaled <Abdelrahman.Khaled16@fci.helwan.edu.eg>
2020-03-30 03:50:40 +0200

    Add Category Class

---
+++ Testgit/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testgit
+{
+    class Category
+    {
+
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
